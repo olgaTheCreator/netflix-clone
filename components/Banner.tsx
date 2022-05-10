b6f6b6ddf0375513b88a55a908016f8c1b1b13d3
ojb <curylo.olga@gmail.com>
2022-05-10 13:33:06 +0100

    working on tutorial

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,9 +1,39 @@
-function Banner() {
+import { Movie } from '../typings'
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import { baseUrl } from '../constants/movie'
+
+interface Props {
+  netflixOriginals: Movie[]
+}
+
+function Banner({ netflixOriginals }: Props) {
+  const [movie, setMovie] = useState<Movie | null>(null)
+
+  useEffect(() => {
+    setMovie(
+      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+    )
+  }, [netflixOriginals])
   return (
-    <div>
-      {/* <div>
-        <Image />
-      </div> */}
+    <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
+      <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
+        <Image
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+      <h1 className="text-2xl md:text-4xl lg:text-7xl ">
+        {movie?.title || movie?.name || movie?.original_name}
+      </h1>
+      <p className="max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
+        {movie?.overview}
+      </p>
+      <div>
+        <button className="bannerButton">Play</button>
+        <button className="bannerButton">More Info</button>
+      </div>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,29 @@ import Head from 'next/head'
 import Header from '../components/Header'
 import Banner from '../components/Banner'
 import requests from '../utils/requests'
+import { Movie } from '../typings'
 
-const Home = () => {
+interface Props {
+  netflixOriginals: Movie[]
+  trendingNow: Movie[]
+  topRated: Movie[]
+  actionMovies: Movie[]
+  comedyMovies: Movie[]
+  horrorMovies: Movie[]
+  romanceMovies: Movie[]
+  documentaries: Movie[]
+}
+
+const Home = ({
+  netflixOriginals,
+  actionMovies,
+  comedyMovies,
+  documentaries,
+  horrorMovies,
+  romanceMovies,
+  topRated,
+  trendingNow,
+}: Props) => {
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
@@ -12,7 +33,7 @@ const Home = () => {
       </Head>
       <Header />
       <main>
-        <Banner />
+        <Banner netflixOriginals={netflixOriginals} />
         <section></section>
       </main>
     </div>
@@ -52,7 +73,6 @@ export const getServerSideProps = async () => {
       horrorMovies: horrorMovies.results,
       romanceMovies: romanceMovies.results,
       documentaries: documentaries.results,
-      products,
     },
   }
 }
